8e98dd84f2458353108f34366af8eb1315090097
Ítalo Henrique <italo.santos@dkp-it.com.br>
2026-04-17 17:18:50 -0300

    feat(backend): bootstrap application with CORS, global prefix and vilidation pipe.

---
+++ backend/src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  await app.listen(3000);
+  console.log('Backend rodando em http://localhost:3000');
+}
+
+bootstrap();
